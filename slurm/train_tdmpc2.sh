75de57647e309b4c1cbfbc2c09837de7d26bc205
Sumeet Batra <sbatra@nvidia.com>
2025-03-10 18:39:41 -0700

    update slurm runner

--- slurm/train_tdmpc2.sh
+++ slurm/train_tdmpc2.sh
@@ -4,14 +4,16 @@
 #SBATCH -n1
 #SBATCH -c4
 #SBATCH --mem-per-cpu=6G
-#SBATCH --output=tmp/tdmpc2_maniskill_%j.log
+#SBATCH --output=tmp/tdmpc2_%j.log
 
 export MUJOCO_GL=egl
+export PATH=$HOME/SlotWM/tdmpc2:$PATH
 
 SEED=0
+ENV="maniskill"
 TASK="pick-cube"
-RUN_NAME="tdmpc2_maniskill_${TASK}_seed${SEED}"
+RUN_NAME="tdmpc2_${ENV}_${TASK}_seed_${SEED}"
 srun python -m tdmpc2.train --task=$TASK \
                             --exp_name=$RUN_NAME \
-                            --wandb_group="maniskill" \
+                            --wandb_group=$ENV \
                             --seed=$SEED
